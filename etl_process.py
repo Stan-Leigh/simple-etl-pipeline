9fdd87bafd37e8fbc8c6899bac72f3aff63a2c7d
Nwani Ugonna Stanley <64145417+Stan-Leigh@users.noreply.github.com>
2023-11-06 06:04:32 -0800

    Add files via upload

--- etl_process.py
+++ etl_process.py
@@ -17,7 +17,8 @@ load_dotenv()
 mysql_password = os.getenv("MYSQL_PASSWORD")
 mongodb_password = os.getenv("MONGODB_PASSWORD")
 
-def etl_pipeline():
+
+def get_data():
     # Extracting data from mysql database
     try:
         connection = msql.connect(host='localhost', database='diamond_data', user='root', password=mysql_password) 
@@ -45,26 +46,41 @@ def etl_pipeline():
             connection.close()
             print('mysql connection is closed')
 
-
     # Creating dataframe
     df = pd.DataFrame(table_rows, columns=["carat", "cut", "color","clarity","depth", "table", "price", "x", "y", "z"])
 
+    return df
+
+
+def clean_data(df):
+    df.dropna(inplace=True)
+
+    return df
+
+
+def append_time_column(df):
     # create new column to keep track of inserted data
     df['inserted_at'] = datetime.now()
 
+    return df
 
+
+def etl_pipeline(df):
     # Making connection to Mongocloud
     cluster = pymongo.MongoClient(f"mongodb://Stanislaus:{mongodb_password}@ac-kddib4z-shard-00-00.wfg4du8.mongodb.net:27017,ac-kddib4z-shard-00-01.wfg4du8.mongodb.net:27017,ac-kddib4z-shard-00-02.wfg4du8.mongodb.net:27017/?ssl=true&replicaSet=atlas-dr51du-shard-0&authSource=admin&retryWrites=true&w=majority")
 
     # creating collection testdb 
     db = cluster["diamond_db"]
     collection = db["diamonds"]
-    
+
     # Inserting values to table test 
     x = collection.insert_many(df.to_dict('records')) # my result comes from mysql cursor
     print(len(x.inserted_ids)) 
 
-    return "Success"
+    return len(x.inserted_ids)
 
 
-etl_pipeline()
+step1 = get_data()
+step2 = clean_data(step1)
+step3 = append_time_column(step2)
+step4 = etl_pipeline(step3)
--- test_etl_process.py
+++ test_etl_process.py
@@ -1,4 +1,28 @@
-from etl_process import etl_pipeline
+from etl_process import clean_data, append_time_column, etl_pipeline
+import pandas as pd
 
-def test_etl_process():
-    assert etl_pipeline() == "Success"
+def test_clean_data():
+    df = pd.read_csv('mock_data.csv')
+    result = clean_data(df)
+
+    assert result.isnull().sum().sum() == 0
+
+
+def test_append_time_column():
+    df = pd.read_csv('mock_data.csv')
+    result = append_time_column(df)
+
+    response = "inserted_at" in result.columns
+
+    assert response == True
+
+
+def test_etl_pipeline(mocker):
+    df = pd.read_csv('mock_data.csv')
+    mocker.patch("etl_process.etl_pipeline", 100)
+
+    expected = 100
+
+    actual = etl_pipeline(df)
+
+    assert expected == actual
